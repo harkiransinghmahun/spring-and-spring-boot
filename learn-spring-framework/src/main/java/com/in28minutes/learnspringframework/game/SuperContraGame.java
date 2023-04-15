da9412e73c12340bd229df62575db71406b797ae
Harkiran Mahun <harkiransinghmahun@gmail.com>
2023-04-15 12:11:30 +0100

    Iteration 1: Tightly coupled GameRunner class

---
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/game/SuperContraGame.java
@@ -0,0 +1,21 @@
+package com.in28minutes.learnspringframework.game;
+
+public class SuperContraGame {
+	
+	public void up() {
+		System.out.println("Go up");
+	}
+	
+	public void down() {
+		System.out.println("Go down");
+	}
+	
+	public void left() {
+		System.out.println("Go back");
+	}
+	
+	public void right() {
+		System.out.println("Shoot a bullet");
+	}
+	
+}
